46103e0dd5943e142760efbe7c96018a7f8b7bdc
Ronaldo García <garciaronaldo20@gmail.com>
2025-07-19 02:51:09 -0500

    Solucionado problema del fondo gris

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -5,8 +5,7 @@ export default function HomeView() {
     return (
         <>
             <Header />
-
-            <main className="bg-gray-100 py-10 min-h-screen bg-no-repeat bg-right-top"
+            <main className="py-10 min-h-screen bg-no-repeat bg-right-top"
                 style={{ backgroundImage: "url('/ground3.svg')" }}
             >
                 <div className=" max-w-5xl mx-auto mt-10">
@@ -15,16 +14,13 @@ export default function HomeView() {
                             Todas tus <span className="text-cyan-400">Redes Sociales </span>
                             en un enlace
                         </h1>
-
                         <p className="text-white text-xl">
                             Únete a más de 200 mil developers compartiendo sus redes sociales, comparte tu perfil de TikTok, Facebook, Instagram, YouTube, Github y más
                         </p>
-
                         <SearchForm />
                     </div>
                 </div>
             </main>
-
         </>
     )
 }
